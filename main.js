f027a7d8c456d90927a625c618cbcbde0d4852f6
0xyz3n <itz.broken.xd@gmail.com>
2024-11-07 21:46:09 +0530

    update

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 document.onreadystatechange = function () {
-            if (document.readyState !== "complete") {
-                document.querySelector(
-                    "body").style.visibility = "hidden";
-                document.querySelector("#loader").style.visibility="visible";
-            } else {
-                document.querySelector("#loader").style.display = "none";
-                document.querySelector("body").style.visibility = "visible";
-            }
-        };
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility="visible";
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
   
 ScrollReveal({
   reset: true,
